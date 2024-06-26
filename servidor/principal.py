d27c1d3ee14528bf9146919582dbd63713ed9c71
JorgeMeR <JorgeMeR@github.com>
2024-06-26 23:53:52 +0200

    Changing / to give the feedback as a response, adding RESTful endpoint /hello for Hello, World

--- servidor/principal.py
+++ servidor/principal.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app=Flask(__name__)
 
-@app.route("/")
+feedbacks=[
+	{ 
+	"title": "Experienced salespeople",
+	"name": "Alex H.",
+	"date": "2020-02-02",
+	"feedback": "It was great to talk to the salespeople in the team"
+	}
+]
+
+@app.route("/hello")
 def hello_world():
 	return "Hello, World!"
+
+@app.route("/")
+def get_feedbacks():
+	return jsonify(feedbacks)
